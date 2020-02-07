f9a6b47f8cb05cf0ec370e74d26f4ee79d0b6dab
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-07 11:25:21 +0700

    Add 4 new activities

---
+++ app/src/main/java/com/mike/miwok/FamilyActivity.java
@@ -0,0 +1,13 @@
+package com.mike.miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+}
