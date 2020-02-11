c332ec8f20228c9af4eab92d6279ecfb15ef4b4d
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-11 12:59:48 +0700

    Add images to proper activities

--- app/src/main/java/com/mike/miwok/ColorsActivity.java
+++ app/src/main/java/com/mike/miwok/ColorsActivity.java
@@ -16,14 +16,14 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.words_list);
 
         ArrayList<Word> colors = new ArrayList<>(Arrays.asList(
-                new Word("red", "weṭeṭṭi"),
-                new Word("green", "chokokki"),
-                new Word("brown", "ṭakaakki"),
-                new Word("gray", "ṭopoppi"),
-                new Word("black", "kululli"),
-                new Word("white", "kelelli"),
-                new Word("dusty yellow", "ṭopiisә"),
-                new Word("mustard yellow", "chiwiiṭә")));
+                new Word("red", "weṭeṭṭi", R.drawable.color_red),
+                new Word("green", "chokokki", R.drawable.color_green),
+                new Word("brown", "ṭakaakki", R.drawable.color_brown),
+                new Word("gray", "ṭopoppi", R.drawable.color_gray),
+                new Word("black", "kululli", R.drawable.color_black),
+                new Word("white", "kelelli", R.drawable.color_white),
+                new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow),
+                new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow)));
 
         WordAdapter wordAdapter = new WordAdapter(this, colors);
 
