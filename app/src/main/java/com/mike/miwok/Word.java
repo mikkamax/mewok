0b506876e964075f2cc636b060f6954c446511c2
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-11 12:56:34 +0700

    Adapt item lists for the images

--- app/src/main/java/com/mike/miwok/Word.java
+++ app/src/main/java/com/mike/miwok/Word.java
@@ -1,19 +1,32 @@
 package com.mike.miwok;
 
+import javax.xml.transform.Source;
+
 public class Word {
     private String mEnglishWord;
     private String mMiwokWord;
+    private int mImageId;
 
     public Word(String englishWord, String miwokWord) {
         mEnglishWord = englishWord;
         mMiwokWord = miwokWord;
     }
 
-    public String getmEnglishWord() {
+    public Word(String englishWord, String miwokWord, int imageId) {
+        mEnglishWord = englishWord;
+        mMiwokWord = miwokWord;
+        mImageId = imageId;
+    }
+
+    public String getEnglishWord() {
         return mEnglishWord;
     }
 
     public String getMiwokWord() {
         return mMiwokWord;
+    }
+
+    public int getImageId() {
+        return mImageId;
     }
 }
--- app/src/main/java/com/mike/miwok/WordAdapter.java
+++ app/src/main/java/com/mike/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,9 +26,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         Word currentWord = getItem(position);
         TextView miwok = listItemView.findViewById(R.id.item_miwok);
         TextView english = listItemView.findViewById(R.id.item_english);
+        ImageView image = listItemView.findViewById(R.id.item_image);
 
         miwok.setText(currentWord.getMiwokWord());
-        english.setText(currentWord.getmEnglishWord());
+        english.setText(currentWord.getEnglishWord());
+        image.setImageResource(currentWord.getImageId());
 
         return listItemView;
     }
