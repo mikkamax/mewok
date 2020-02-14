772048a12adb9bc8deb512c02946c8d33d51ed68
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-14 11:58:46 +0700

    Add: all clickable item's show a pressed state when clicked on

--- app/src/main/java/com/mike/miwok/WordAdapter.java
+++ app/src/main/java/com/mike/miwok/WordAdapter.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
     private int mColorResourceId;
-    private MediaPlayer mediaPlayer;
 
     public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
