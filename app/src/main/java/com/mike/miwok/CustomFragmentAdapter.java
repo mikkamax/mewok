8b310e4e9e221f0f5e048e9f5544e99d1d21659e
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-18 12:29:31 +0700

    Refactor: left only main activity which is using ViewPager and Fragments

---
+++ app/src/main/java/com/mike/miwok/CustomFragmentAdapter.java
@@ -0,0 +1,33 @@
+package com.mike.miwok;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class CustomFragmentAdapter extends FragmentPagerAdapter {
+    public CustomFragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0 :
+                return new NumbersFragment();
+            case 1 :
+                return new FamilyFragment();
+            case 2 :
+                return new ColorsFragment();
+            case 3 :
+                return new PhrasesFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/com/mike/miwok/MainActivity.java
+++ app/src/main/java/com/mike/miwok/MainActivity.java
@@ -4,21 +4,17 @@ package com.mike.miwok;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MainClickListener mainClickListener = new MainClickListener();
-
-        findViewById(R.id.numbers).setOnClickListener(mainClickListener);
-        findViewById(R.id.colors).setOnClickListener(mainClickListener);
-        findViewById(R.id.family).setOnClickListener(mainClickListener);
-        findViewById(R.id.phrases).setOnClickListener(mainClickListener);
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new CustomFragmentAdapter(getSupportFragmentManager()));
     }
 }
