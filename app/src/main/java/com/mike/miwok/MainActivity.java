e67eb5737452233e7170bf1d4fd7fac1244e649f
mikkamax <59081705+mikkamax@users.noreply.github.com>
2020-02-18 17:50:38 +0700

    Add: self-made backstack for fragments via viewpager

--- app/src/main/java/com/mike/miwok/MainActivity.java
+++ app/src/main/java/com/mike/miwok/MainActivity.java
@@ -8,16 +8,57 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.Stack;
+
 
 public class MainActivity extends AppCompatActivity {
+    protected Stack<Integer> mFragmentBackStack = new Stack<>();
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    protected boolean isBackButtonPressed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = findViewById(R.id.viewpager);
-        viewPager.setAdapter(new CustomFragmentAdapter(getSupportFragmentManager()));
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new CustomFragmentAdapter(getSupportFragmentManager()));
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
+        mTabLayout = findViewById(R.id.tab_layout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        // creating backstack-a-like for the fragments via viewpager
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if (!isBackButtonPressed) {
+                    mFragmentBackStack.push(tab.getPosition());
+                }
+                else {
+                    isBackButtonPressed = false;
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        isBackButtonPressed = true;
+        if (!mFragmentBackStack.empty()) {
+            mViewPager.setCurrentItem(mFragmentBackStack.pop());
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
